0557e2e71943308afc29ee98238ee05bc19432ab
Torben Döscher <mail@tdoescher.de>
2022-08-09 10:50:55 +0200

    Update

--- src/Resources/public/cokiban.js
+++ src/Resources/public/cokiban.js
@@ -1,4 +1,5 @@
 document.addEventListener('alpine:init', () => {
+    // eslint-disable-next-line no-undef
     Alpine.store('cokiban', {
         id: null,
         name: '',
@@ -11,94 +12,94 @@ document.addEventListener('alpine:init', () => {
         cache: {},
         valid: {},
         initialize (config) {
-            const alpine = this
-            alpine.id = config.id
-            alpine.name = config.name
-            alpine.version = config.version
-            alpine.days = config.days
-            alpine.active = config.active
+            const alpine = this;
+            alpine.id = config.id;
+            alpine.name = config.name;
+            alpine.version = config.version;
+            alpine.days = config.days;
+            alpine.active = config.active;
             if (Array.isArray(config.cookies)) {
                 config.cookies.forEach((item) => {
-                    alpine.cache[item] = false
-                })
+                    alpine.cache[item] = false;
+                });
             }
-            alpine.valid = Object.assign({}, alpine.cache)
-            alpine.pages = config.pages
-            alpine.load()
+            alpine.valid = Object.assign({}, alpine.cache);
+            alpine.pages = config.pages;
+            alpine.load();
             if (alpine.active === false) {
-                return
+                return;
             }
             if (alpine.date === null || (alpine.days && (alpine.date + alpine.days * 86400000) < new Date().getTime())) {
-                alpine.openBanner()
+                alpine.openBanner();
             }
         },
         load () {
-            const alpine = this
-            let storage = localStorage.getItem(alpine.name)
+            const alpine = this;
+            let storage = localStorage.getItem(alpine.name);
             try {
-                storage = JSON.parse(storage)
+                storage = JSON.parse(storage);
             } catch {
-                storage = null
+                storage = null;
             }
             if (storage !== null && storage.version === alpine.version) {
-                alpine.date = storage.date
+                alpine.date = storage.date;
                 Object.keys(storage.cookies).forEach((item) => {
                     if (alpine.cache[item] !== undefined) {
-                        alpine.cache[item] = storage.cookies[item]
+                        alpine.cache[item] = storage.cookies[item];
                     }
-                })
-                alpine.valid = Object.assign({}, alpine.cache)
+                });
+                alpine.valid = Object.assign({}, alpine.cache);
             }
         },
         save () {
-            const alpine = this
-            alpine.valid = Object.assign({}, alpine.cache)
+            const alpine = this;
+            alpine.valid = Object.assign({}, alpine.cache);
             localStorage.setItem(alpine.name, JSON.stringify({
                 id: alpine.id,
                 version: alpine.version,
                 date: new Date().getTime(),
                 cookies: alpine.valid
-            }))
-            alpine.closeBanner()
+            }));
+            alpine.closeBanner();
         },
         acceptAll () {
-            const alpine = this
+            const alpine = this;
             Object.keys(alpine.cache).forEach((item) => {
-                alpine.cache[item] = true
-            })
-            clearInterval(this.counter)
-            alpine.save()
+                alpine.cache[item] = true;
+            });
+            clearInterval(this.counter);
+            alpine.save();
         },
         saveSettings () {
-            const alpine = this
-            alpine.save()
+            const alpine = this;
+            alpine.save();
         },
         switchCookie (cookie) {
-            const alpine = this
-            const group = cookie.split(/[A-Z]/)[0]
-            let groupActive = true
+            const alpine = this;
+            const group = cookie.split(/[A-Z]/)[0];
+            let groupActive = true;
             Object.keys(alpine.cache).forEach((item) => {
                 if (item !== cookie && item.startsWith(cookie)) {
-                    alpine.cache[item] = alpine.cache[cookie]
+                    alpine.cache[item] = alpine.cache[cookie];
                 }
                 if (item !== group && item.startsWith(group) && !alpine.cache[item]) {
-                    groupActive = false
+                    groupActive = false;
                 }
-            })
-            alpine.cache[group] = groupActive
+            });
+            alpine.cache[group] = groupActive;
         },
         showDetails () {
-            const alpine = this
-            alpine.details = !this.details
+            const alpine = this;
+            alpine.details = !this.details;
         },
         openBanner () {
-            const alpine = this
-            alpine.details = false
-            alpine.show = true
+            const alpine = this;
+            alpine.details = false;
+            alpine.show = true;
         },
         closeBanner () {
-            const alpine = this
-            alpine.show = false
+            const alpine = this;
+            alpine.show = false;
         }
-    })
-})
+    });
+});
