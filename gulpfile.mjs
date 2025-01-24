4f7215a79f963ead09eb0e8d50326f7edce5f769
Torben Döscher <mail@tdoescher.de>
2025-01-24 12:26:06 +0100

    Update

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,29 +1,28 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import url from 'node:url'
 import gulp from 'gulp'
+import autoprefixer from 'gulp-autoprefixer'
 import esbuild from 'gulp-esbuild'
 import { sassPlugin } from 'esbuild-sass-plugin'
 
-const style = () => {
-  return gulp.src(['src/Resources/public/cokiban.scss'])
-  .pipe(esbuild({
-    plugins: [ sassPlugin() ],
-    minify: true,
-    outfile: 'cokiban.min.css',
-  }))
-  .pipe(gulp.dest('src/Resources/public/'))
-}
-
-const script = () => {
+const scripts = () => {
   return gulp.src(['src/Resources/public/cokiban.js'])
-  .pipe(esbuild({
-    bundle: true,
-    drop: ['debugger', 'console'],
-    minify: true,
-    outfile: 'cokiban.min.js',
-  }))
-  .pipe(gulp.dest('src/Resources/public/'))
+    .pipe(esbuild({
+      bundle: true,
+      drop: ['debugger', 'console'],
+      minify: true,
+      outfile: 'cokiban.min.js',
+    }))
+    .pipe(gulp.dest('src/Resources/public/'))
 }
 
-gulp.task('build', gulp.parallel(style, script))
+const styles = () => {
+  return gulp.src(['src/Resources/public/cokiban.scss'])
+    .pipe(esbuild({
+      plugins: [ sassPlugin() ],
+      minify: true,
+      outfile: 'cokiban.min.css',
+    }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('src/Resources/public/'))
+}
+
+gulp.task('build', gulp.parallel(scripts, styles))
